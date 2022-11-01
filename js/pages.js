214d868fdd62b14f78c03c4d9478dbfc72b22432
madmansion4 <madmansion4@gmail.com>
2022-11-01 23:41:54 +0900

    수정4

--- js/pages.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $("#header-area").load("/header.html");
-    $("#footer-area").load("/footer.html");
-});
-
-// function fnTop() {
-//     var offset = $("#top").offset();
-//     $('html, body').animate({ scrollTop: offset.top }, 200);
-// }
